0c83e0221af5f3f9536f8d488d3d143a7b95d96a
0xBlackRaven <90768535+0xBlackRaven@users.noreply.github.com>
2023-11-05 20:14:00 +0100

    Patch and enhancing code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use druid::widget::Label;
 use druid::{AppLauncher, Widget, WindowDesc, Data, Env};
-use std::String;
+use std::string::String;
+use std::thread;
+use core::time::Duration;
 
 // On Windows platform, don't show a console when opening the app.
-#![windows_subsystem = "windows"]
+#[windows_subsystem = "windows"]
 
 #[derive(Clone, Data)]
 struct AppData {
@@ -11,14 +13,14 @@ struct AppData {
 }
 
 fn build_ui() -> impl Widget<AppData> {
-    Label::new(|data: &AppData, _: &Env| &AppData::main_test_label)
+    Label::new(|data: &AppData, _: &Env| data.main_test_label.to_string())
 }
 
 fn main() {
     let main_window = WindowDesc::new(build_ui())
         .title("Tough Cookie");
-    let initial_data = AppData(main_test_label: "");
-    let launcher = AppLauncher::with_window(main_window)
+    let initial_data = AppData{main_test_label: "".to_string()};
+    let launcher = AppLauncher::with_window(main_window);
 
     // If we want to create commands from another thread `launcher.get_external_handle()`
     // should be used. For sending commands from within widgets you can always call
@@ -38,24 +40,22 @@ fn main() {
 }
 
 fn main_thread(event_sink: druid::ExtEventSink) {
-    let mut label = String::new();
     let final_label = "You should improve your security <3";
 
-    loop {
-        //check if both labels have the same lenght
-        if label.len() >= final_label.len() {
-            break;
-        } else {
-            //add the next character
-            label += final_label[label.len()+1];
-        }
-
-        // schedule idle callback to change the data
+    // Initializing the window to prevent characters from not being displayed
+    event_sink.add_idle_callback(move |data: &mut AppData| {
+        data.main_test_label.push(' ');
+    });
+    std::thread::sleep(Duration::from_millis(1000));
+    
+    for chars in final_label.chars() {
+        // Add the character
         event_sink.add_idle_callback(move |data: &mut AppData| {
-            *data.main_test_label = label;
+            data.main_test_label.push(chars);
         });
 
-        //wait a bit
-        thread::sleep(Duration::from_millis(1*1000));
+
+        // Wait a bit
+        std::thread::sleep(Duration::from_millis(100));
     }
 }
